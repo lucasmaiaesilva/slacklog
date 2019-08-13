cec8280a8710c2513c06a175b0396fda3c960071
lucasmaiaesilva <lucasmaiaesilva@gmail.com>
2019-08-13 01:54:35 -0300

    feat: add cors exception

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const app = express();
 const port = require('./config/data').port;
 
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD");
+  // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // set routes
 app.use(require('./app/routes'));
