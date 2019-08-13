c52f1b6c9fa6ecb11f5e228df510a87ea5de7660
lucasmaiaesilva <lucasmaiaesilva@gmail.com>
2019-08-13 01:58:31 -0300

    fix: fix bug cors

--- index.js
+++ index.js
@@ -3,16 +3,12 @@ require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const port = require('./config/data').port;
 
+app.use(cors());
 app.use(bodyParser.json());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD");
-  // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 // set routes
 app.use(require('./app/routes'));
